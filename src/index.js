eb6e184b056da8e13c98c4f24f906354069ac1a6
sastubblefield <stubblefield.sa@gmail.com>
2021-09-25 20:20:42 -0700

    Finished CSS design

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let apiKey = "73bb802b6842545f8bc067782928d7ae";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=San Diego&appid={apiKey}&units=imperial";
+console.group(apiUrl);
