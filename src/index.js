49af0621418a99732a057d5fc5a9eaa3a532be0f
sastubblefield <stubblefield.sa@gmail.com>
2021-09-25 22:00:09 -0700

    API Integration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector(`#temperature`);
+  let locationElement = document.querySelector(`#location`);
+  let humidityElement = document.querySelector(`#humidity`);
+  let windElement = document.querySelector(`#wind`);
+  let dateElement = document.querySelector(`#date`);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  locationElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
 let apiKey = "73bb802b6842545f8bc067782928d7ae";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=San Diego&appid={apiKey}&units=imperial";
-console.group(apiUrl);
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+axios.get(apiUrl).then(displayTemperature);
